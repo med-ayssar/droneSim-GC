961c987e45414310c917977483c417c86c56ae84
Ayssar <mohamedb@miltenyibiotec.de>
2026-07-20 18:50:04 +0200

    Fix deps

--- px4-autopilot/default.nix
+++ px4-autopilot/default.nix
@@ -14,10 +14,23 @@ let
     doCheck = false;
   };
 
+  # PX4 uses the empy 3.x API (em.RAW_OPT etc.), which was removed in empy 4.x.
+  # nixpkgs ships 4.x, so pin 3.3.4 here.
+  empy3 = pkgs.python3Packages.buildPythonPackage rec {
+    pname = "empy";
+    version = "3.3.4";
+    format = "setuptools";
+    src = pkgs.fetchurl {
+      url = "https://files.pythonhosted.org/packages/3b/95/88ed47cb7da88569a78b7d6fb9420298df7e99997810c844a924d96d3c08/empy-3.3.4.tar.gz";
+      hash = "sha256-c6xJeFtgFHnfTqGKfHm8EwSop8NMArlHLPEgauiPAbM=";
+    };
+    doCheck = false;
+  };
+
   # Single Python interpreter carrying every module PX4's build imports.
   pythonEnv = pkgs.python3.withPackages (ps: [
     pyros-genmsg   # genmsg
-    ps.empy
+    empy3          # empy 3.3.4 (NOT nixpkgs' 4.x)
     ps.jinja2
     ps.kconfiglib
     ps.numpy
