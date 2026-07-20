90a05c51d9ccb218c3803b50f5b5991759d8a6b5
med-ayssar <med.ayssar@gmail.com>
2026-07-20 13:30:10 +0200

    add git for buildInput

--- micro-xrce-dds-agent/default.nix
+++ micro-xrce-dds-agent/default.nix
@@ -12,13 +12,14 @@
     src = pkgs.fetchgit {
       url = "https://github.com/eProsima/Micro-XRCE-DDS-Agent.git";
       rev = "v3.0.1";
-      hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+      hash = "sha256-nBJ+WuoZhB3+/NiYAH/l1r0BK1aFzAUfGpyOKpWC1sg=";
     };
 
 
     nativeBuildInputs = [
       pkgs.cmake
       pkgs.pkg-config
+      pkgs.git
     ];
 
 
