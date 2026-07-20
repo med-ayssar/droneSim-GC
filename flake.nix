e22cfa5d721fc1c38fcb4ed06bfc358de451314f
med-ayssar <med.ayssar@gmail.com>
2026-07-20 17:27:29 +0200

    Appy changes to whole file

--- flake.nix
+++ flake.nix
@@ -1,43 +1,77 @@
 {
   description = "PX4 ROS2 development environment";
 
+
   inputs = {
-    nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
-    nixpkgs.follows = "nix-ros-overlay/nixpkgs";
+
+    nix-ros-overlay.url =
+      "github:lopsided98/nix-ros-overlay/master";
+
+    nixpkgs.follows =
+      "nix-ros-overlay/nixpkgs";
+
   };
 
-  outputs = { self, nix-ros-overlay, nixpkgs }:
 
-    nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    { self, nix-ros-overlay, nixpkgs }:
+
+    nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem
+      (system:
 
       let
 
-        pkgs = import nixpkgs {
-          inherit system;
+        pkgs =
+          import nixpkgs {
 
-          overlays = [
-            nix-ros-overlay.overlays.default
-          ];
+            inherit system;
 
-          config.allowUnfree = true;
-        };
+            overlays = [
+              nix-ros-overlay.overlays.default
+            ];
+
+            config.allowUnfree = true;
+
+          };
 
 
+        #
+        # ROS Humble package set with foonathan_memory_vendor fix
+        #
+        rosPackages =
+          pkgs.rosPackages.humble.overrideScope
+            (final: prev: {
+
+              foonathan-memory-vendor =
+                prev.foonathan-memory-vendor.overrideAttrs
+                  (old: {
+
+                    NIX_CFLAGS_COMPILE =
+                      (old.NIX_CFLAGS_COMPILE or "")
+                      + " -Wno-error=deprecated-literal-operator";
+
+                  });
+
+            });
+
+
+        #
+        # ROS2 Humble environment
+        #
         ros2 =
-          with pkgs.rosPackages.humble;
+          with rosPackages;
           buildEnv {
+
             underlay = true;
 
             paths = [
+
               ros-core
-              colcon
               ros-base
-              # add more ROS packages here:
-              # rclcpp
-              # rclpy
-              # geometry-msgs
-              # sensor-msgs
+              colcon
+
             ];
+
           };
 
 
@@ -81,9 +115,13 @@
               name = "px4-stack";
 
               paths = [
+
                 micro-xrce-dds-agent.package
+
                 px4-msgs.package
+
                 px4-autopilot.package
+
               ];
 
             };
@@ -91,50 +129,57 @@
         };
 
 
-        devShells.default = pkgs.mkShell {
 
-          name = "PX4 ROS2 Humble development environment";
+        devShells.default =
+          pkgs.mkShell {
 
-
-          packages = [
-
-            ros2
-
-            micro-xrce-dds-agent.package
-
-            px4-msgs.package
-
-            px4-autopilot.package
+            name =
+              "PX4 ROS2 Humble development environment";
 
 
-            pkgs.colcon
+            packages = [
 
-            pkgs.git
+              ros2
 
-            pkgs.tmux
+              micro-xrce-dds-agent.package
 
-          ];
+              px4-msgs.package
+
+              px4-autopilot.package
 
 
-          shellHook = ''
+              pkgs.colcon
 
-            echo ""
-            echo "PX4 ROS2 Humble environment"
-            echo ""
+              pkgs.git
 
-            export ROS_DOMAIN_ID=0
+              pkgs.tmux
 
-            alias start-agent="
-              MicroXRCEAgent udp4 -p 8888
-            "
+            ];
 
-            alias start-px4="
-              px4
-            "
 
-          '';
 
-        };
+            shellHook = ''
+
+              echo ""
+              echo "PX4 ROS2 Humble environment"
+              echo ""
+
+
+              export ROS_DOMAIN_ID=0
+
+
+              alias start-agent="
+                MicroXRCEAgent udp4 -p 8888
+              "
+
+
+              alias start-px4="
+                px4
+              "
+
+            '';
+
+          };
 
       });
 
@@ -142,12 +187,18 @@
   nixConfig = {
 
     extra-substituters = [
+
       "https://ros.cachix.org"
+
     ];
 
+
     extra-trusted-public-keys = [
+
       "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo="
+
     ];
 
   };
+
 }
