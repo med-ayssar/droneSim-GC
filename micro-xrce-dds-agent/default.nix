bd0677c124e760d375c9212ee10f851ba008d697
med-ayssar <med.ayssar@gmail.com>
2026-07-20 13:34:14 +0200

    change version

--- micro-xrce-dds-agent/default.nix
+++ micro-xrce-dds-agent/default.nix
@@ -11,7 +11,7 @@
 
     src = pkgs.fetchgit {
       url = "https://github.com/eProsima/Micro-XRCE-DDS-Agent.git";
-      rev = "v3.0.1";
+      rev = "v2.4.3";
       hash = "sha256-nBJ+WuoZhB3+/NiYAH/l1r0BK1aFzAUfGpyOKpWC1sg=";
     };
 
@@ -30,13 +30,11 @@
 
 
     buildPhase = ''
-      mkdir build
-      cd build
-
-      cmake .. \
-        -DCMAKE_BUILD_TYPE=Release
-
-      make -j$NIX_BUILD_CORES
+    mkdir build && \
+    cd build && \
+    cmake .. && \
+    make -j"$(nproc)" && \
+    make install &&
     '';
 
 
